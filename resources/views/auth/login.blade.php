5d59944d1cdba8ffc7823ebb4492382af1384d16
anggitpp <anggitpp23@gmail.com>
2023-04-07 10:42:41 +0700

    implement new login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -59,11 +59,15 @@ License: For each use you must have a valid license purchased only from above li
 <script>var defaultThemeMode = "light"; var themeMode; if ( document.documentElement ) { if ( document.documentElement.hasAttribute("data-theme-mode")) { themeMode = document.documentElement.getAttribute("data-theme-mode"); } else { if ( localStorage.getItem("data-theme") !== null ) { themeMode = localStorage.getItem("data-theme"); } else { themeMode = defaultThemeMode; } } if (themeMode === "system") { themeMode = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"; } document.documentElement.setAttribute("data-theme", themeMode); }</script>
 <!--end::Theme mode setup on page load-->
 <!--begin::Root-->
-<div class="d-flex flex-column flex-root" id="kt_app_root">
+<div class="d-flex flex-column flex-root" id="kt_app_root" >
     <!--begin::Authentication - Sign-in -->
     <div class="d-flex flex-column flex-lg-row flex-column-fluid">
         <!--begin::Body-->
-        <div class="d-flex flex-column flex-lg-row-fluid w-lg-50 p-10 order-2 order-lg-1">
+        <div class="d-flex flex-lg-row-fluid  w-100 w-lg-50 bgi-size-contain bgi-position-center order-1 order-lg-1"
+             style="background-image: url(assets/media/misc/auth-bg-kengloss.png); background-size: contain; background-repeat: no-repeat;background-color: #F6FAFC !important;"
+        >
+        </div>
+        <div class="d-flex flex-column flex-lg-row-fluid w-100 w-lg-50 p-10 order-2 order-lg-2">
             <!--begin::Form-->
             <div class="d-flex flex-center flex-column flex-lg-row-fluid">
                 <!--begin::Wrapper-->
@@ -176,31 +180,7 @@ License: For each use you must have a valid license purchased only from above li
         </div>
         <!--end::Body-->
         <!--begin::Aside-->
-        <div class="d-flex flex-lg-row-fluid w-lg-50 bgi-size-cover bgi-position-center order-1 order-lg-2"
-             style="background-image: url(assets/media/misc/auth-bg-backup.png)">
-            <!--begin::Content-->
-            <div class="d-flex flex-column flex-center py-7 py-lg-15 px-5 px-md-15 w-100">
-                <!--begin::Logo-->
-{{--                <a href="../../demo1/dist/index.html" class="mb-0 mb-lg-12">--}}
-{{--                    <img alt="Logo" src="assets/media/misc/custom-1.png" class="h-60px h-lg-75px" />--}}
-{{--                </a>--}}
-                <!--end::Logo-->
-                <!--begin::Image-->
-                <img class="d-none d-lg-block mx-auto w-275px w-md-50 w-xl-500px mb-10 mb-lg-20" src="assets/media/misc/auth-screens.png" alt="" />
-                <!--end::Image-->
-                <!--begin::Title-->
-                <h1 class="d-none d-lg-block text-white fs-2qx fw-bolder text-center mb-7">Fast, Efficient and Productive</h1>
-                <!--end::Title-->
-                <!--begin::Text-->
-                <div class="d-none d-lg-block text-white fs-base text-center">In this kind of post,
-                    <a href="#" class="opacity-75-hover text-warning fw-bold me-1">the blogger</a>introduces a person they’ve interviewed
-                    <br />and provides some background information about
-                    <a href="#" class="opacity-75-hover text-warning fw-bold me-1">the interviewee</a>and their
-                    <br />work following this is a transcript of the interview.</div>
-                <!--end::Text-->
-            </div>
-            <!--end::Content-->
-        </div>
+
         <!--end::Aside-->
     </div>
     <!--end::Authentication - Sign-in-->
