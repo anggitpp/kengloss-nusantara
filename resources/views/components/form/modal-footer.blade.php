458cceb624a304a8f811607f3e094073a6b6b269
anggitpp <anggitpp23@gmail.com>
2023-04-09 22:29:39 +0700

    feat : change icon modal button

--- resources/views/components/form/modal-footer.blade.php
+++ resources/views/components/form/modal-footer.blade.php
@@ -1,6 +1,6 @@
 <div class="modal-footer flex-end">
     <button type="button" class="btn btn-primary" id="btnSubmit" data-kt-users-modal-action="submit">
-        <span class="indicator-label"><i class="fas fa-print"></i>Simpan</span>
+        <span class="indicator-label"><i class="fa-regular fa-floppy-disk"></i> Simpan</span>
         <span class="indicator-progress">Please wait...
         <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
     </button>
