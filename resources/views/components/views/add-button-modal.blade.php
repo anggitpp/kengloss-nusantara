45f16044a7432f8f8de9799f53f24ad8ef7a0251
anggitpp <anggitpp23@gmail.com>
2023-04-09 22:27:38 +0700

    feat : implement back button to submit button modal

--- resources/views/components/views/add-button-modal.blade.php
+++ resources/views/components/views/add-button-modal.blade.php
@@ -1,4 +1,9 @@
-@props(['route' => '', 'text' => '', 'class' => ''])
-<button {{ $attributes->merge(['class' => 'btn btn-primary btn-modal '.$class]) }} data-bs-toggle="modal" data-url="{{ $route }}">
+@props(['route' => '', 'text' => '', 'class' => '', 'filter' => '', 'size' => 'mw-650px', 'withBack' => false, 'url_back' => ''])
+@if($withBack)
+    <a href="{{ !empty($url_back) ? $url_back : url()->previous() }}" class="btn btn-light-primary mx-3">
+        <i class="fa-solid fa-arrow-left"></i> Kembali
+    </a>
+@endif
+<button {{ $attributes->merge(['class' => 'btn btn-primary btn-modal '.$class]) }} data-bs-toggle="modal" data-url="{{ $route }}" data-filter="{{ $filter }}" data-size="{{ $size }}">
     <i class="fa-solid fa-plus"></i> {{ $text }}
 </button>
