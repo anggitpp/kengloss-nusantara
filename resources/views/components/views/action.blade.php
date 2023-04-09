47dccc5d5f8e999069744fe1f24dfd4b2869fffb
anggitpp <anggitpp23@gmail.com>
2023-04-09 22:26:44 +0700

    feat : implement slot, action v2

--- resources/views/components/views/action.blade.php
+++ resources/views/components/views/action.blade.php
@@ -1,33 +1,76 @@
-@props(['url_edit' => '', 'url_destroy' => '', 'url_show' => '', 'isModal' => true, 'url_slot' => '', 'icon_slot' => 'fa-solid fa-list', 'isModalSlot' => true, 'menu_path' => ''])
+@props(['url_edit' => '', 'url_destroy' => '', 'url_show' => '', 'icon_show' => 'fa-solid fa-list', 'isModal' => true, 'url_slot' => '', 'icon_slot' => 'fa-solid fa-list', 'isModalSlot' => true, 'menu_path' => '', 'customAction' => ''])
 <div class="text-center justify-content-between">
+    @if(is_array($customAction))
+        @foreach($customAction as $key => $action)
+            @if(str_contains($action['url'], 'edit'))
+                @can('edit '.$menu_path)
+                    <a
+                            @if($action['isModal'])
+                                data-url="{{ $action['url'] }}"
+                            @else
+                                href="{{ $action['url'] }}"
+                            @endif
+                            class="btn btn-icon btn-light-{{ $action['class-icon'] ?? 'info' }} w-30px h-30px me-1 {{ $action['isModal'] ? 'btn-modal' : '' }}"
+                            title="Edit Data"
+                            @if($action['isModal'])
+                                data-bs-toggle="modal"
+                            @endif
+                    >
+                        <i class="{{ $action['icon'] }}"></i>
+                    </a>
+                @endcan
+            @elseif(str_contains($action['url'], 'destroy'))
+                @can('delete '.$menu_path)
+                    <button href="{{ $action['url'] }}" id="delete" class="btn btn-icon btn-light-danger w-30px h-30px" title="Delete Data">
+                        <i class="{{ $action['icon'] }}"></i>
+                    </button>
+                @endcan
+            @else
+                <a
+                        @if($action['isModal'])
+                            data-url="{{ $action['url'] }}"
+                        @else
+                            href="{{ $action['url'] }}"
+                        @endif
+                        class="btn btn-icon btn-light-{{ $action['class-icon'] ?? 'info' }} w-30px h-30px me-1 {{ $action['isModal'] ? 'btn-modal' : '' }}"
+                        @if($action['isModal'])
+                            data-bs-toggle="modal"
+                        @endif
+                        title="{{ $action['title'] }}"
+                >
+                    <i class="{{ $action['icon'] }}"></i>
+                </a>
+            @endif
+        @endforeach
+    @endif
     @if($url_slot)
         <a
-            @if($isModalSlot)
-                data-url="{{ $url_slot }}"
-            @else
-                href="{{ $url_slot }}"
-            @endif
-            class="btn btn-icon btn-light-info w-30px h-30px me-1 {{ $isModalSlot ? 'btn-modal' : '' }}"
-            @if($isModalSlot)
-                data-bs-toggle="modal"
-            @endif
+                @if($isModalSlot)
+                    data-url="{{ $url_slot }}"
+                @else
+                    href="{{ $url_slot }}"
+                @endif
+                class="btn btn-icon btn-light-info w-30px h-30px me-1 {{ $isModalSlot ? 'btn-modal' : '' }}"
+                @if($isModalSlot)
+                    data-bs-toggle="modal"
+                @endif
         >
             <i class="{{ $icon_slot }}"></i>
         </a>
     @endif
     @if($url_show)
-        <a href="{{ $url_show }}" class="btn btn-icon btn-light-success w-30px h-30px me-1">
-            <i class="fa-solid fa-list"></i>
+        <a href="{{ $url_show }}" class="btn btn-icon btn-light-info w-30px h-30px me-1" title="List Data">
+            <i class="{{ $icon_show }}"></i>
         </a>
     @endif
     @if($url_edit)
         @can('edit '.$menu_path)
             @if($isModal)
-                <a data-bs-toggle="modal" class="btn btn-icon btn-light-primary w-30px h-30px me-1 btn-modal" data-url="{{ $url_edit }}">
+                <a data-bs-toggle="modal" title="Edit Data" class="btn btn-icon btn-light-primary w-30px h-30px me-1 btn-modal" data-url="{{ $url_edit }}">
                     <i class="fa-solid fa-pen"></i>
                 </a>
             @else
-                <a href="{{ $url_edit }}" class="btn btn-icon btn-light-primary w-30px h-30px me-1">
+                <a href="{{ $url_edit }}" title="Edit Data" class="btn btn-icon btn-light-primary w-30px h-30px me-1">
                     <i class="fa-solid fa-pen"></i>
                 </a>
             @endif
@@ -35,7 +78,7 @@
     @endif
     @if($url_destroy)
         @can('delete '.$menu_path)
-            <button href="{{ $url_destroy }}" id="delete" class="btn btn-icon btn-light-danger w-30px h-30px">
+            <button href="{{ $url_destroy }}" id="delete" class="btn btn-icon btn-light-danger w-30px h-30px" title="Delete Data">
                 <i class="fa-solid fa-trash"></i>
             </button>
         @endcan
